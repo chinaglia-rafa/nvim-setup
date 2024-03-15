4582a92bdd28bbd27f7b6127d173b4f611eb3af7
chinaglia <chinaglia.rafa@gmail.com>
2024-03-15 19:05:52 -0300

    config estável, pelo menos

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,12 +1,20 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- add dracula
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
+    lazy = false,
+    opts = {
+      transparent_bg = true,
+      italic_comment = true,
+    },
+  },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "dracula",
     },
   },
 }
